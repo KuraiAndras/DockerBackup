7fd4ce1af1f58642e91ab0fa744c89d0b453197e
Kurai András <k.andras9642@gmail.com>
2024-01-18 22:19:16 +0100

    add Docker.DotNet, list images

--- src/DockerBackup/Worker.cs
+++ src/DockerBackup/Worker.cs
@@ -1,24 +1,33 @@
-namespace DockerBackup
+using Docker.DotNet;
+
+namespace DockerBackup;
+
+public class Worker : BackgroundService
 {
-    public class Worker : BackgroundService
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
     {
-        private readonly ILogger<Worker> _logger;
+        _logger = logger;
+    }
 
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var client = new DockerClientConfiguration()
+            .CreateClient();
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        var images = await client.Images.ListImagesAsync(new() { All = true }, stoppingToken);
+
+        _logger.LogInformation("Images: {images}", images.SelectMany(i => i.RepoTags));
+
+        while (!stoppingToken.IsCancellationRequested)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            if (_logger.IsEnabled(LogLevel.Information))
             {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
+
+            await Task.Delay(1000, stoppingToken);
         }
     }
 }
