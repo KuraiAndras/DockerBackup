be6fb341decd121dd881466176e6c937e88ce226
Kurai András <k.andras9642@gmail.com>
2025-01-25 14:25:11 +0100

    update migrations

---
+++ src/DockerBackup.WebApi/Migrations/20250125132024_UpdateEf9-0-1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DockerBackup.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEf901 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
