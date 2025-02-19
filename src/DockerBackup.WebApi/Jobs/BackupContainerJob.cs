5fdb9d12b8f14c19108f340a1c7892025fb6d237
Kurai András <k.andras9642@gmail.com>
2025-02-19 23:52:56 +0100

    Move backup to domain service

--- src/DockerBackup.WebApi/Jobs/BackupContainerJob.cs
+++ src/DockerBackup.WebApi/Jobs/BackupContainerJob.cs
@@ -1,27 +1,17 @@
-using System.IO.Compression;
-
 using Docker.DotNet;
 
-using DockerBackup.WebApi.Database;
 using DockerBackup.WebApi.Domain;
 using DockerBackup.WebApi.Extensions;
-using DockerBackup.WebApi.Options;
 
 using Hangfire;
-
-using Microsoft.Extensions.Options;
 
 namespace DockerBackup.WebApi.Jobs;
 
 public sealed class BackupContainerJob
 (
-    IOptions<ServerOptions> serverOptions,
-    IOptions<BackupOptions> backupOptions,
-    ILogger<BackgroundJob> logger,
     IDockerClient docker,
     IRecurringJobManager recurringJob,
-    TimeProvider timeProvider,
-    ApplicationDb db
+    IContainerBackupService containerBackupService
 )
 {
     public record Parameters(string ContainerName, List<string>? Directories = null);
@@ -30,97 +20,15 @@ public sealed class BackupContainerJob
 
     public async Task DoBackup(Parameters parameters, CancellationToken cancellationToken = default)
     {
-        var backupFilePaths = new List<FileInfo>();
-        ContainerBackupInfo? container = null;
+        var containerResponse = await docker.Containers.InspectContainerSafeAsync(parameters.ContainerName, cancellationToken);
 
-        try
+        if (containerResponse is null)
         {
-            var now = timeProvider.GetUtcNow().UtcDateTime;
+            recurringJob.RemoveIfExists(parameters.ContainerName);
 
-            var containerResponse = await docker.Containers.InspectContainerSafeAsync(parameters.ContainerName, cancellationToken);
-
-            if (containerResponse is null)
-            {
-                recurringJob.RemoveIfExists(parameters.ContainerName);
-
-                return;
-            }
-
-            container = new ContainerBackupInfo(containerResponse, backupOptions.Value);
-
-            var backupDirectory = container.GetBackupDirectory(serverOptions.Value, now);
-
-            if (!Directory.Exists(backupDirectory))
-            {
-                Directory.CreateDirectory(backupDirectory);
-            }
-
-            if (container.IsRunning)
-            {
-                await docker.Containers.StopContainerAsync(container.Id, new()
-                {
-                    WaitBeforeKillSeconds = container.WaitBeforeKill,
-                }, cancellationToken);
-            }
-
-            var backedUpContainerPaths = new List<string>();
-
-            foreach (var containerPathToBackUp in parameters.Directories is { Count: > 0 }
-                ? parameters.Directories
-                : container.Directories)
-            {
-                await using DockerArchive archive = await docker.Containers.GetArchiveFromContainerAsync(container.Id, new()
-                {
-                    Path = containerPathToBackUp,
-                }, statOnly: false, cancellationToken);
-
-                var tarFileName = ContainerBackupInfo.GetBackupFileName(backupDirectory, containerPathToBackUp, container.Compress);
-                backupFilePaths.Add(new(tarFileName));
-
-                await using var tarStream = container.Compress
-                    ? new GZipStream(File.Create(tarFileName), container.CompressionMode)
-                    : File.Create(tarFileName) as Stream;
-
-                await archive.Stream.CopyToAsync(tarStream, cancellationToken);
-
-                backedUpContainerPaths.Add(containerPathToBackUp);
-            }
-
-            var containerBackup = new ContainerBackup
-            {
-                ContainerName = parameters.ContainerName,
-                CreatedAt = now,
-                Files = [.. backedUpContainerPaths.Select((containerPathToBackUp, i) => FileBackup.Create
-                (
-                    filePath: backupFilePaths[i].FullName,
-                    containerPath: containerPathToBackUp,
-                    sizeInBytes: backupFilePaths[i].Length
-                ))],
-            };
-            db.ContainerBackups.Add(containerBackup);
-
-            await db.SaveChangesAsync(cancellationToken);
+            return;
         }
-        catch (Exception e)
-        {
-            foreach (var backupFilePath in backupFilePaths)
-            {
 
-                if (backupFilePath.Exists)
-                {
-                    backupFilePath.Delete();
-                }
-            }
-
-            logger.LogError(e, "Creating backup {ContainerName} failed", parameters.ContainerName);
-            throw;
-        }
-        finally
-        {
-            if (container?.IsRunning == true)
-            {
-                await docker.Containers.StartContainerAsync(container.Id, new(), cancellationToken);
-            }
-        }
+        await containerBackupService.CreateBackup(parameters.ContainerName, parameters.Directories, cancellationToken);
     }
 }
